073d0aefdc7d8661b1c3129d3061f7b631a3b557
Joachim <muchaiminjoachim@gmail.com>
2026-04-22 09:41:15 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
-int sum(int N, int arr[]){
+int sum(int N, int *arr[]){
     int sum = 0;
     for (int j =0; j<N; j++){
-        sum = sum + arr[j];
+        sum = sum + *arr[j];
     }
     return sum;
 }
 
-float avg(int N, int arr[]){
+float avg(int N, int *arr[]){
     float sum = 0;
     for (int j =0; j<N; j++){
-        sum = sum + arr[j];
+        sum = sum + *arr[j];
     }
     float avg = sum/N;
     return avg;
 
 }
 
-int max(int N, int arr[]){
+int max(int N, int *arr[]){
     int max = -10;
     for (int j = 0; j<N; j++){
-        if(max < arr[j]){
-            max = arr[j];
+        if(max < *arr[j]){
+            max = *arr[j];
         }
     }
     return max;
 }
 
-int idx(int N, int arr[]){
+int idx(int N, int *arr[]){
     int max = -10;
     int idx = 0;
     for (int j = 0; j<N; j++){
-        if(max < arr[j]){
-            max = arr[j];
+        if(max < *arr[j]){
+            max = *arr[j];
             idx = j;
         }
     }
@@ -45,21 +45,24 @@ int main(){
     int N;
     scanf("%d", &N);
 
+
     int a[N];
+    int *ptr[N];
     for( int i = 0; i<N; i++){
         scanf("%d", &a[i]);
+        ptr[i]= &a[i];
     }
 
-    int hasil1 = sum(N, a);
+    int hasil1 = sum(N, ptr);
     printf("\nSUM %d\n", hasil1);
 
-    float hasil2 = avg(N, a);
+    float hasil2 = avg(N, ptr);
     printf("AVG %.2f\n", hasil2);
 
-    int hasil3 = max(N, a);
+    int hasil3 = max(N, ptr);
     printf("MAX %d\n", hasil3);
 
-    int hasil4 = idx(N, a);
+    int hasil4 = idx(N, ptr);
     printf("IDX %d\n", hasil4);
 
 
